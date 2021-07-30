c9e02785adb7f17edc1de4f4c7459860493e3dfb
gammam <42178449+gammam@users.noreply.github.com>
2021-07-30 14:33:56 +0200

    add snippet base on starting name ( pa,psp,nodo)

--- api-definitions/openapi/add_snippet.sh
+++ api-definitions/openapi/add_snippet.sh
@@ -4,9 +4,37 @@
 FILE=$1
 echo $FILE
 
+case $FILE in 
+
+psp*)
+sed -i.bak '4s/.*/  x-logo: \
+    backgroundColor: '#FFFFFF' \
+    url: "https:\/\/www.pagopa.gov.it\/assets\/images\/pagopa-logo.png" \
+  description: \
+      $ref: descriptionPSP.md/' \
+${FILE}
+;;
+
+node*)
+
 sed -i.bak '4s/.*/  x-logo: \
     backgroundColor: '#FFFFFF' \
     url: "https:\/\/www.pagopa.gov.it\/assets\/images\/pagopa-logo.png" \
   description: \
       $ref: description.md/' \
 ${FILE}
+
+;;
+
+pa*)
+sed -i.bak '4s/.*/  x-logo: \
+    backgroundColor: '#FFFFFF' \
+    url: "https:\/\/www.pagopa.gov.it\/assets\/images\/pagopa-logo.png" \
+  description: \
+      $ref: descriptionPA.md/' \
+${FILE}
+;;
+
+
+esac
+
